ad45233da91ace7c53046206201dd8d1f38ee7c1
bhavesh <bhaveshvaishnav555@gmail.com>
2023-12-12 19:28:23 +0530

    assignment 1,2 done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,12 @@
 */
 
 function isAnagram(str1, str2) {
-
+  for (let i = 0; i < str1.length; i++) {
+    let char = str1[i];
+    if (!str2.includes(char)) {
+      return false;
+    }
+  }
 }
 
 module.exports = isAnagram;
