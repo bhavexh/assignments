0c82a59f8ed0b79624ba4ca67addfd0656712513
bhavesh <bhaveshvaishnav555@gmail.com>
2023-12-13 00:38:39 +0530

    solved week1 easy and medium

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -9,5 +9,15 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    let sum = 0;
+    for(let i = 0; i <= n; i++) {  
+        sum += i;
+    }
 }
+const beforeDate = new Date();
+const beforeTime = beforeDate.getTime();
+calculateTime(10000000000);
+const afterDate = new Date();
+const afterTime = afterDate.getTime();
+const timeTaken = afterTime - beforeTime;
+console.log(timeTaken);
