601efe39c6f61d2dcdcc8bfeb5009202c5d490ad
bhavesh <bhaveshvaishnav555@gmail.com>
2024-01-12 23:05:26 +0530

    week 6 use-memo and use-callback assginment done

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
-import { Assignment1 } from './components/Assignment2'
-import { Assignment2 } from './components/Assignment1'
-import Assignment3 from './components/Assignment3'
+import Assignment1 from './components/Assignment1.jsx'
+import Assignment2 from './components/Assignment2.jsx'
+import Assignment3 from './components/Assignment3.jsx'
 
 function App() {
   return (
     <>
       <Assignment1 />
-      {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment2 />
+      <Assignment3 />
     </>
   )
 }
--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,29 +1,53 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+  const [items, setItems] = useState([
+    { name: "Chocolates", value: 10 },
+    { name: "Chips", value: 20 },
+    { name: "Onion", value: 30 },
+    { name: "Tomato", value: 30 },
+    // Add more items as needed
+  ]);
+  const [itemName, setItemName] = useState("");
+    const [itemValue, setItemValue] = useState("");
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+    const addHandler = () => {
+        setItems([...items, {name: itemName, value: itemValue}])
+        setItemName("");
+        setItemValue(0);
+    }
+
+  // Your code starts here
+  let totalValue = 0;
+  totalValue = useMemo(() => {
+    let sum = 0;
+    for (let i = 0; i < items.length; i++) {
+      sum = sum + items[i].value;
+    }
+    return sum;
+  }, [items]);
+  // Your code ends here
+  return (
+    <div>
+      <div>
+        <h4>Item Name</h4>
+        <input type="text" value={itemName} onChange={(e)=>{setItemName(e.target.value)}}/>
+        <h4>Price</h4>
+        <input type="number" value={itemValue} onChange={(e)=>{setItemValue(Number(e.target.value))}}/>
+        <button onClick={addHandler}>add item</button>
+      </div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.name} - Price: ${item.value}
+          </li>
+        ))}
+      </ul>
+      <p>Total Value: {totalValue}</p>
+    </div>
+  );
 };
 
 export default Assignment3;
